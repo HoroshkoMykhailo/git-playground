aa4a63aabbffe2efe47ff0f7fbfcd2194170680d
g.isachenko <george.isachenko@tatemgames.com>
2020-02-08 18:11:43 +0200

    Change stub file reader to return more numbers

--- nice-project/EmptyFileReader.cs
+++ nice-project/EmptyFileReader.cs
@@ -6,7 +6,11 @@ namespace NiceProject
     {
         public IEnumerable<int> ReadAllNumbers()
         {
+<<<<<<< HEAD:nice-project/EmptyFileReader.cs
             return new int[0];
+=======
+            return new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+>>>>>>> 85e9acc (Change stub file reader to return more numbers):nice-project/StubFileReader.cs
         }
     }
 }
